030050d7d09dec99214b749c2749d9647ef02f0e
Adil-Akothiat <adilrar156@gmail.com>
2023-02-04 13:11:04 +0100

    :))

--- src/components/pages/blogs/blogs.js
+++ src/components/pages/blogs/blogs.js
@@ -1,6 +1,7 @@
 import React from "react";
 import BlogsView from "./components/blogs/blogsView";
 import "../../../styles/pages/blogs.css";
+import BlogsData from "../../../data/blogs.json";
 
 export default function Blogs () {
     return (
@@ -9,7 +10,7 @@ export default function Blogs () {
                 <div className="m-title m-title-top mb-5 text-capitalize">
                     <h2 className="fw-normal text-main">recent blogs</h2>
                 </div>
-                <BlogsView />
+                <BlogsView blogs={BlogsData}/>
             </div>
         </div>
     );
--- src/components/pages/blogs/components/blogs/blogsView.js
+++ src/components/pages/blogs/components/blogs/blogsView.js
@@ -1,142 +1,38 @@
 import React from "react";
 import * as Unicons from "@iconscout/react-unicons";
 
-export default function BlogsView () {
+export default function BlogsView (props) {
     return (
         <div className="blogs-view">
             <div className="row">
-                <div className="col-xl-4 col-md-6 col-sm-6 mb-4">
-                    <div className="card">
-                        <div className="overflow-hidden"><img className="img-fluid rounded-top" src={"https://picsum.photos/200/301"} alt="name"/> </div>
-                        <div className="card-body">
-                            <div className="d-flex align-items-center justify-content-between mb-3">
-                                <span className="rounded text-capitalize">technology</span>
-                                <div className="d-flex align-items-center date"> 
-                                    <Unicons.UilCalendarAlt />
-                                    <p className="fw-light">14th February 2020</p>
+                {
+                    props.blogs.map((b, i)=> (
+                    <div key={"key-"+i} className="col-xl-4 col-md-6 col-sm-6 mb-4">
+                        <div className="card">
+                            <div className="overflow-hidden rounded-top">
+                                <img className="img-fluid rounded-top" src={b.image} alt={b.category}/>
+                            </div>
+                            <div className="card-body">
+                                <div className="d-flex align-items-center justify-content-between mb-3">
+                                    <span className="rounded text-capitalize">{b.category}</span>
+                                    <div className="d-flex align-items-center date"> 
+                                        <Unicons.UilCalendarAlt />
+                                        <p className="fw-light">{b.date}</p>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="blog-title">
-                                <h3 className="fw-bolder text-capitalize">Finding The Intersection Of Social Justice And Tech</h3>
-                            </div>
-                            <div className="blog-description">
-                                <p className="fw-light">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor in cididunt ut labore et dolore
-                                </p>
+                                <div className="blog-title">
+                                    <h3 className="fw-bolder text-capitalize">{b.title}</h3>
+                                </div>
+                                <div className="blog-description">
+                                    <p className="fw-light">
+                                        {b.description}
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="col-xl-4 col-md-6 col-sm-6 mb-4">
-                    <div className="card">
-                        <div className="overflow-hidden"><img className="img-fluid rounded-top" src={"https://picsum.photos/200/302"} alt="name"/> </div>
-                        <div className="card-body">
-                            <div className="d-flex align-items-center justify-content-between mb-3">
-                                <span className="rounded text-capitalize">technology</span>
-                                <div className="d-flex align-items-center date"> 
-                                    <Unicons.UilCalendarAlt />
-                                    <p className="fw-light">14th February 2020</p>
-                                </div>
-                            </div>
-                            <div className="blog-title">
-                                <h3 className="fw-bolder text-capitalize">Finding The Intersection Of Social Justice And Tech</h3>
-                            </div>
-                            <div className="blog-description">
-                                <p className="fw-light">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor in cididunt ut labore et dolore
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-xl-4 col-md-6 col-sm-6 mb-4">
-                    <div className="card">
-                        <div className="overflow-hidden"><img className="img-fluid rounded-top" src={"https://picsum.photos/200/303"} alt="name"/> </div>
-                        <div className="card-body">
-                            <div className="d-flex align-items-center justify-content-between mb-3">
-                                <span className="rounded text-capitalize">technology</span>
-                                <div className="d-flex align-items-center date"> 
-                                    <Unicons.UilCalendarAlt />
-                                    <p className="fw-light">14th February 2020</p>
-                                </div>
-                            </div>
-                            <div className="blog-title">
-                                <h3 className="fw-bolder text-capitalize">Finding The Intersection Of Social Justice And Tech</h3>
-                            </div>
-                            <div className="blog-description">
-                                <p className="fw-light">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor in cididunt ut labore et dolore
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-xl-4 col-md-6 col-sm-6 mb-4">
-                    <div className="card">
-                        <div className="overflow-hidden"><img className="img-fluid rounded-top" src={"https://picsum.photos/200/304"} alt="name"/> </div>
-                        <div className="card-body">
-                            <div className="d-flex align-items-center justify-content-between mb-3">
-                                <span className="rounded text-capitalize">technology</span>
-                                <div className="d-flex align-items-center date"> 
-                                    <Unicons.UilCalendarAlt />
-                                    <p className="fw-light">14th February 2020</p>
-                                </div>
-                            </div>
-                            <div className="blog-title">
-                                <h3 className="fw-bolder text-capitalize">Finding The Intersection Of Social Justice And Tech</h3>
-                            </div>
-                            <div className="blog-description">
-                                <p className="fw-light">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor in cididunt ut labore et dolore
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-xl-4 col-md-6 col-sm-6 mb-4">
-                    <div className="card">
-                        <div className="overflow-hidden"><img className="img-fluid rounded-top" src={"https://picsum.photos/200/305"} alt="name"/> </div>
-                        <div className="card-body">
-                            <div className="d-flex align-items-center justify-content-between mb-3">
-                                <span className="rounded text-capitalize">technology</span>
-                                <div className="d-flex align-items-center date"> 
-                                    <Unicons.UilCalendarAlt />
-                                    <p className="fw-light">14th February 2020</p>
-                                </div>
-                            </div>
-                            <div className="blog-title">
-                                <h3 className="fw-bolder text-capitalize">Finding The Intersection Of Social Justice And Tech</h3>
-                            </div>
-                            <div className="blog-description">
-                                <p className="fw-light">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor in cididunt ut labore et dolore
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-xl-4 col-md-6 col-sm-6 mb-4">
-                    <div className="card">
-                        <div className="overflow-hidden"><img className="img-fluid rounded-top" src={"https://picsum.photos/200/306"} alt="name"/> </div>
-                        <div className="card-body">
-                            <div className="d-flex align-items-center justify-content-between mb-3">
-                                <span className="rounded text-capitalize">technology</span>
-                                <div className="d-flex align-items-center date"> 
-                                    <Unicons.UilCalendarAlt />
-                                    <p className="fw-light">14th February 2020</p>
-                                </div>
-                            </div>
-                            <div className="blog-title">
-                                <h3 className="fw-bolder text-capitalize">Finding The Intersection Of Social Justice And Tech</h3>
-                            </div>
-                            <div className="blog-description">
-                                <p className="fw-light">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor in cididunt ut labore et dolore
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                    ))
+                }
             </div>
         </div>
     );
